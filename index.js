8f489a12912146b0259ad15d7daff5c267198dc7
sandhya563 <sandhyas19@navgurukul.org>
2022-09-02 17:26:40 +0530

    post api insert multiple entry

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-
-const express = require('express');
-const userRoutes = require('./src/routes/user')
-const userInfoRoutes = require('./src/routes/userInfo')
-const userHobbiRoutes = require('./src/routes/userhobbis')
+const express = require("express");
+const userRoutes = require("./src/routes/user");
+const userInfoRoutes = require("./src/routes/userInfo");
+const userHobbiRoutes = require("./src/routes/userhobbis");
 const db = require("./src/models/index");
 
 const app = express();
 db.sequelize.sync();
 app.use(express.json());
 
-app.use("/usersData", userRoutes)
-app.use("/userInfoData", userInfoRoutes)
-app.use("/usersHobbiData", userHobbiRoutes)
-
+app.use("/usersData", userRoutes);
+app.use("/userInfoData", userInfoRoutes);
+app.use("/usersHobbiData", userHobbiRoutes);
 
 app.listen(3000, () => {
-  console.log('server is runing on 3000 port')
+  console.log("server is runing on 3000 port");
 });
-
